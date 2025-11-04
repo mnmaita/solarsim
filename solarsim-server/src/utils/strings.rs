fafd88dece37bf9a07ebda69685da356e6697152
mnmaita <47983254+mnmaita@users.noreply.github.com>
2025-11-04 16:35:12 +0100

    Add string utils

---
+++ solarsim-server/src/utils/strings.rs
@@ -0,0 +1,12 @@
+pub fn capitalize(str: impl Into<String>) -> String {
+    str.into()
+        .split('_')
+        .map(|word| {
+            let mut chars = word.chars();
+            chars.next().map_or(String::default(), |char| {
+                char.to_uppercase().collect::<String>() + chars.as_str()
+            })
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
