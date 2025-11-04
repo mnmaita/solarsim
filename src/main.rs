4bd15dc3bb9337fcbea028ff13b0ac8a6e6f9837
mnmaita <47983254+mnmaita@users.noreply.github.com>
2025-11-04 16:35:09 +0100

    Bevy hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_systems(Update, hello_world_system).run();
+}
+
+fn hello_world_system(mut commands: Commands) {
+    println!("hello world");
+
+    commands.spawn((
+        Node {
+            width: percent(100),
+            height: percent(100),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(Text::new("Hello World!"))],
+    ));
 }
