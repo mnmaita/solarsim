3051a76d08e5d6e7a9a174210dada16ebace90f8
mnmaita <47983254+mnmaita@users.noreply.github.com>
2025-11-04 16:35:11 +0100

    Add requests.ts file

---
+++ solarsim-client/src/app/requests.ts
@@ -0,0 +1,82 @@
+import type { JSONValue } from "next/dist/server/config-shared";
+
+const host = process.env.NEXT_PUBLIC_SOLARSIM_SERVER_HOST;
+const port = process.env.NEXT_PUBLIC_SOLARSIM_SERVER_PORT;
+const url = URL.parse(`${host}:${port}`) ?? `${host}:${port}`;
+const requestInit: RequestInit = { method: "post" };
+
+type BRPMethod =
+  | "world.get_components"
+  | "world.query"
+  | "world.spawn_entity"
+  | "world.despawn_entity"
+  | "world.remove_components"
+  | "world.insert_components"
+  | "world.mutate_components"
+  | "world.reparent_entities"
+  | "world.list_components"
+  | "world.get_components+watch"
+  | "world.list_components+watch"
+  | "world.get_resources"
+  | "world.insert_resources"
+  | "world.remove_resources"
+  | "world.mutate_resources"
+  | "world.list_resources"
+  | "registry.schema"
+  | "rpc.discover";
+
+interface BRPCommonBody {
+  jsonrpc: "2.0";
+  id: JSONValue;
+}
+
+interface BRPCommonRequestBody extends BRPCommonBody {
+  jsonrpc: "2.0";
+  method: BRPMethod;
+  id: JSONValue;
+}
+
+export interface BRPGetResourcesRequest extends BRPCommonRequestBody {
+  method: "world.get_resources";
+  params: {
+    resource: string;
+  };
+}
+
+export interface BRPGetResourcesResponse extends BRPCommonBody {
+  result: {
+    value: unknown;
+  };
+}
+
+/**
+ * Get the value of a Resource
+ *
+ * @param {string} name - Type name of the Resource (without its full path).
+ * @param {string} [path] - Fully-qualified type path of the resource to get (without its name).
+ * Defaults to "solarsim_server".
+ * @param {JSONValue} [id] - Arbitrary JSON data. The server completely ignores its contents,
+ * and the client may use it for any purpose. It will be copied via
+ * serialization and deserialization (so object property order, etc. can’t be
+ * relied upon to be identical) and sent back to the client as part of the
+ * response. Defaults to 0.
+ * */
+export function getResource(
+  name: string,
+  path: string = "solarsim_server",
+  id: JSONValue = 0
+): Promise<BRPGetResourcesResponse> {
+  const body: BRPGetResourcesRequest = {
+    jsonrpc: "2.0",
+    method: "world.get_resources",
+    id,
+    params: {
+      resource: `${path}::${name}`,
+    },
+  };
+
+  return fetch(url, {
+    ...requestInit,
+    body: JSON.stringify(body),
+  }).then((res) => res.json() as Promise<BRPGetResourcesResponse>);
+}
