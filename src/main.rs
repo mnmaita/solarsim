9fe9024a96728296b741a2f171a26e782b4c6fa0
mnmaita <47983254+mnmaita@users.noreply.github.com>
2025-11-04 16:35:10 +0100

    Split UI and println systems

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world_system).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello_world_ui_system)
+        .add_systems(Update, hello_world_system)
+        .run();
 }
 
-fn hello_world_system(mut commands: Commands) {
+fn hello_world_system() {
     println!("hello world");
+}
 
+fn hello_world_ui_system(mut commands: Commands) {
     commands.spawn((
         Node {
             width: percent(100),
